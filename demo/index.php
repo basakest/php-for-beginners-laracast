86f90b62c64b24535571d858f35e88e9a64e504d
basakest <ab1652759879@gmail.com>
2024-04-30 11:47:23 +0800

    pdo try

--- demo/index.php
+++ demo/index.php
@@ -1,4 +1,18 @@
 <?php
 
 require 'functions.php';
-require 'route.php';
+// require 'route.php';
+
+$dsn = 'mysql:dbname=myapp;host=127.0.0.1';
+$user = 'root';
+$password = '';
+$pdo = new PDO($dsn, $user, $password);
+
+$statement = $pdo->prepare('SELECT * FROM post');
+$statement->execute();
+$res = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($res as $post) {
+    echo '<li>' . $post['title'] . '</li>';
+}
+
