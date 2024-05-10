72ce39da8f7dc39cabb9e3bc4d9aab9593bd8889
basakest <ab1652759879@gmail.com>
2024-05-10 17:35:03 +0800

    form validation

--- demo/controllers/note-create.php
+++ demo/controllers/note-create.php
@@ -3,10 +3,19 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $databaseConfig = (require 'config.php')['database'];
     $db = new Database($databaseConfig);
-    $db->query('INSERT INTO `notes` (`body`, `user_id`) VALUES (:body, :user_id)', [
-        'body'    => $_POST['body'],
-        'user_id' => 1,
-    ]);
+    $errors = [];
+    if (strlen($_POST['body']) == 0) {
+        $errors['body'] = 'Note body is required';
+    }
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'Note body can not be more than 1,000 characters';
+    }
+    if (empty($errors)) {
+        $db->query('INSERT INTO `notes` (`body`, `user_id`) VALUES (:body, :user_id)', [
+            'body'    => $_POST['body'],
+            'user_id' => 1,
+        ]);
+    }
 }
 $heading = 'Create Note';
 require 'views/note-create.view.php';
