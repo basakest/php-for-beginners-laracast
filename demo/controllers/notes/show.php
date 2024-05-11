b15603a359747fb384d4ce4305f4e9e81c55b278
basakest <ab1652759879@gmail.com>
2024-05-11 23:03:18 +0800

    add delete button to note

--- demo/controllers/notes/show.php
+++ demo/controllers/notes/show.php
@@ -4,11 +4,17 @@ use Core\Database;
 
 $databaseConfig = (require base_path('config.php'))['database'];
 $db = new Database($databaseConfig);
-$id = $_GET['id'];
+// permission validate
 $currentUserId = 1;
+$id = $_SERVER['REQUEST_METHOD'] == 'GET' ? $_GET['id'] : $_POST['id'];
 $note = $db->query('SELECT * FROM notes WHERE `id` = :id', ['id' => $id])->findOrFail();
-
 authorize($currentUserId === $note['user_id']);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $db->query('delete from `notes` where `id` = :id', ['id' => $_POST['id']]);
+    header('Location: /notes');
+    exit();
+}
 
 view('notes/show.view.php', [
     'heading' => 'My Notes',
--- demo/views/notes/show.view.php
+++ demo/views/notes/show.view.php
@@ -4,12 +4,15 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <!-- Your content -->
         <p class="mb-6">
             <a href="/notes" class="text-blue-500 underline">go back...</a>
         </p>
         <p><?= /** @var array $note */
             htmlspecialchars($note['body']) ?></p>
+        <form class="mt-6" method="post">
+            <button class="text-sm text-red-500">Delete</button>
+            <input type="hidden" name="id" value="<?= $note['id']; ?>">
+        </form>
     </div>
 </main>
 <?php require base_path('views/partials/footer.php') ?>
